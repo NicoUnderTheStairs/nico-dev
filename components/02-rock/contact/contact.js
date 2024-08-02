ea735f4f0d0901a27e998da35912f8140b79c2c1
Nico Fortino <88322851+NicoUnderTheStairs@users.noreply.github.com>
2024-08-02 11:23:51 +0200

    contact, privacy policy

--- components/02-rock/contact/contact.js
+++ components/02-rock/contact/contact.js
@@ -7,6 +7,9 @@ class Contact extends Base {
     super(el);
     gsap.registerPlugin(ScrollTrigger);
 
+    const contactOpen = el.querySelector('.contact__open');
+    const formWrapper = el.querySelector('.contact__form__wrapper');
+
     gsap.set(el.querySelector('.contact__talk--1'), {
       x: '-100%',
     });
@@ -14,8 +17,9 @@ class Contact extends Base {
     gsap.to(el.querySelector('.contact__talk--1'), {
       x: '0%',
       scrollTrigger: {
-        scrub: 0.5,
-        delay: 0.5,
+        scrub: true,
+        delay: 1,
+        ease: 'back.out(1.7)',
         trigger: el,
         start: 'top bottom',
         end: 'bottom top',
@@ -29,13 +33,22 @@ class Contact extends Base {
     gsap.to(el.querySelector('.contact__talk--2'), {
       x: '-100%',
       scrollTrigger: {
-        scrub: 0.5,
-        delay: 0.5,
+        scrub: true,
+        delay: 1,
+        ease: 'back.out(1.7)',
         trigger: el,
         start: 'top bottom',
         end: 'bottom top',
       },
     });
+
+    contactOpen.onclick = () => {
+      if (formWrapper.classList.contains('d-none')) {
+        formWrapper.classList.remove('d-none');
+        // focus on first input
+        formWrapper.querySelector('input').focus();
+      }
+    };
   }
 }
 
